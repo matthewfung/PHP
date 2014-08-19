bc13d2c75829c5e8fb89f0af054a44a17f981ffa
Matthew Fung <matt.fung89@gmail.com>
2014-08-19 20:18:05 +0100

    Optimized by keeping track of a max variable on each iteration

--- selectionSort.php
+++ selectionSort.php
@@ -16,19 +16,24 @@ function swapValues($array, $index1, $index2){
 }
 
 function selectionSort($array){
-	$min = PHP_INT_MAX;
+	$min = PHP_INT_MAX; 
 	$min_index = 0;
+
+	$max = -PHP_INT_MAX;
+	$max_index = 0;
 	for($i=0; $i<count($array); $i++){
-		$min = $array[$i];
-		$min_index = $i;
 		for($j=$i+1; $j<count($array); $j++){
 			if($array[$j] < $min){
 				$min = $array[$j];
 				$min_index = $j;
 			}
+			if($array[$j] > $max){
+				$max = $array[$j];
+				$max_index = $j;
+			}
 			
 		}
-		$array = swapValues($array, $min_index, $i);
+		$array = swapValues($array, $min_index, $max_index);
 	}
 	return $array;
 }
